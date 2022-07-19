79fd8aed1c6ad863c5082aaf77ccfc8d5064c2a5
MajoBerger <marian.berger@dataquest.sk>
2022-07-19 09:25:41 +0200

    last attempt

--- scripts/serve.ts
+++ scripts/serve.ts
@@ -9,6 +9,7 @@ const appConfig: AppConfig = buildAppConfig();
  * Calls `ng serve` with the following arguments configured for the UI in the app config: host, port, nameSpace, ssl
  */
 child.spawn(
-  `ng serve --host 0.0.0.0 --public-host http://dev-5.pc --port 4000 --serve-path ${appConfig.ui.nameSpace} --ssl false  --disable-host-check`,
+  // `ng serve --host ${appConfig.ui.host} --port ${appConfig.ui.port} --serve-path ${appConfig.ui.nameSpace} --ssl ${appConfig.ui.ssl}`,
+  `ng serve --host ${appConfig.ui.host} --public-host http://dev-5.pc --port ${appConfig.ui.port} --serve-path ${appConfig.ui.nameSpace} --ssl false  --disable-host-check`,
   { stdio: 'inherit', shell: true }
 );
